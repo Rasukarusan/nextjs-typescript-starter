b2da41b1068780ec8a0eb777ed8439f5ca458eb2
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-08-12 18:11:43 +0900

    Fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,16 @@
-// import
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { Container } from '@material-ui/core'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      background: theme.palette.background.default,
+    container: {
+      background: theme.palette.background.paper,
+      minHeight: '100vh',
+      padding: '0 0.5rem',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
   })
 )
@@ -17,9 +22,5 @@ export const Layout = (props: Props) => {
   const { children } = props
   const classes = useStyles()
 
-  return (
-    <Container fixed className={classes.root}>
-      {children}
-    </Container>
-  )
+  return <Container fixed>{children}</Container>
 }
