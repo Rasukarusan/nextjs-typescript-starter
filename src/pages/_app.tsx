d04f7f993c8a619f2c6fe9073854360851592590
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-04-09 15:32:28 +0900

    tailwind導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
+import 'tailwindcss/tailwind.css';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props
